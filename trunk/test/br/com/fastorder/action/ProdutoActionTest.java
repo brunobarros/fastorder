3c2a92f79ece6e30b0286d10225df24a7ce2e3f2
barrosbruno <barrosbruno@d0c2b855-9328-0410-85de-ab0a162d3437>
2007-04-28 22:38:31 +0000

    - Criando classe FastOrderAllTests: suite para executar todos os testes de unidade;

--- trunk/test/br/com/fastorder/action/ProdutoActionTest.java
+++ trunk/test/br/com/fastorder/action/ProdutoActionTest.java
@@ -1,7 +1,10 @@
 package br.com.fastorder.action;
 
-import static org.easymock.EasyMock.*;
-import static org.junit.Assert.*;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Collection;
 
