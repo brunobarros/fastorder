b459e81f5d156cfcc3aba7b2671ac0c567f13c0f
diogo.cabral <diogo.cabral@d0c2b855-9328-0410-85de-ab0a162d3437>
2007-05-22 03:14:24 +0000

    Colocando o resto dos testes no suite.

--- trunk/test/br/com/fastorder/FastOrderAllTests.java
+++ trunk/test/br/com/fastorder/FastOrderAllTests.java
@@ -11,15 +11,21 @@ import br.com.fastorder.action.ContaActionTest;
 import br.com.fastorder.action.ProdutoActionTest;
 import br.com.fastorder.action.TipoProdutoActionTest;
 import br.com.fastorder.action.UsuarioActionTest;
+import br.com.fastorder.dao.hibernate.HibernateContaDaoTest;
+import br.com.fastorder.dao.hibernate.HibernateGenericDaoTest;
+import br.com.fastorder.interceptor.PermissionInterceptorTest;
 import br.com.fastorder.model.ContaTest;
 import br.com.fastorder.util.Md5Test;
 
 @RunWith(Suite.class)
 @SuiteClasses( { 
 	ContaActionTest.class, 
-	TipoProdutoActionTest.class, 
-	ProdutoActionTest.class , 
-	UsuarioActionTest.class, 
+	ProdutoActionTest.class ,	
+	TipoProdutoActionTest.class,  
+	UsuarioActionTest.class,
+	HibernateContaDaoTest.class,
+	HibernateGenericDaoTest.class,
+	PermissionInterceptorTest.class,
 	ContaTest.class, 
 	Md5Test.class} )
 public class FastOrderAllTests {
